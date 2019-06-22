179e3ee28bc4e7acf4d360573d28084728b5fdee
rdgao <r.dg.gao@gmail.com>
2019-06-21 22:35:06 -0700

    updated monkey notebook with better dataframe aggregating and all plots

--- utils.py
+++ utils.py
@@ -40,7 +40,9 @@ def return_fg_fits(fg_file, fg_folder):
     fg = FOOOFGroup()
     fg.load(fg_file, fg_folder)
     aps = fg.get_params('aperiodic_params') # get aperiodic parameters
-    pks = fg.get_params('peak_params')
+    # need to resave the old fooof files for this loading to work 
+    #pks = fg.get_params('peak_params')
+    pks = []
     err = fg.get_params('error')
     r2s = fg.get_params('r_squared')
     return aps, pks, err, r2s
@@ -58,37 +60,37 @@ def convert_knee_val(knee, exponent=2.):
     knee_tau = 1./(2*np.pi*knee_freq)
     return knee_freq, knee_tau
 
-def compute_aggregate(df, aggr_func, group_col, loud=False):
-    """
-    Compute one- or two-level aggregate over the dataframe.
-    """
-    if len(group_col)==2:
-        if loud: print('-> aggregating over %s.'%group_col[0])
-        # group by specified column first and aggregate across specified types
-        group_agg = pd.DataFrame()
-        for group_id, group in df.groupby(group_col[0][0]):
-            group_agg = group_agg.append(group.groupby(group_col[0][1]).aggregate(aggr_func))
-
-        if loud: print('---> aggregating over %s.'%group_col[1])
-        grand_agg = group_agg.groupby(group_col[1])
-
-    elif len(group_col)==1 or type(group_col) is str:
-        if type(group_col) is str:
-            group_col = [group_col]
-        # no first-level grouping, directly average across all electrodes
-        if loud: print('-> aggregating over %s.'%group_col[0])
-        group_agg = None
-        grand_agg = df.groupby(group_col[0])
-    return grand_agg, group_agg
-
-def compute_avg_sem(grouped_by, feature, aggr_func):
-    """
-    Computes group average via aggr_func, and standard error.
-    grouped_by must be a grouped_by dataframe
-    """
-    agg_avg = grouped_by[feature].apply(eval(aggr_func))
-    agg_sem = grouped_by[feature].apply(np.nanstd)/(grouped_by[feature].count()**0.5)
-    return agg_avg, agg_sem
+# def compute_aggregate(df, aggr_func, group_col, loud=False):
+#     """
+#     Compute one- or two-level aggregate over the dataframe.
+#     """
+#     if len(group_col)==2:
+#         if loud: print('-> aggregating over %s.'%group_col[0])
+#         # group by specified column first and aggregate across specified types
+#         group_agg = pd.DataFrame()
+#         for group_id, group in df.groupby(group_col[0][0]):
+#             group_agg = group_agg.append(group.groupby(group_col[0][1]).aggregate(aggr_func))
+#
+#         if loud: print('---> aggregating over %s.'%group_col[1])
+#         grand_agg = group_agg.groupby(group_col[1])
+#
+#     elif len(group_col)==1 or type(group_col) is str:
+#         if type(group_col) is str:
+#             group_col = [group_col]
+#         # no first-level grouping, directly average across all electrodes
+#         if loud: print('-> aggregating over %s.'%group_col[0])
+#         group_agg = None
+#         grand_agg = df.groupby(group_col[0])
+#     return grand_agg, group_agg
+#
+# def compute_avg_sem(grouped_by, feature, aggr_func):
+#     """
+#     Computes group average via aggr_func, and standard error.
+#     grouped_by must be a grouped_by dataframe
+#     """
+#     agg_avg = grouped_by[feature].apply(eval(aggr_func))
+#     agg_sem = grouped_by[feature].apply(np.nanstd)/(grouped_by[feature].count()**0.5)
+#     return agg_avg, agg_sem
 
 def plot_psd_fits(f_axis, psds, chan, fgs, fg_labels):
     C_ORD = plt.rcParams['axes.prop_cycle'].by_key()['color']
