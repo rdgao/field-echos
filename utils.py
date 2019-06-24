877ab80d93917ab352a0ee8bf99cd218966e87c6
rdgao <r.dg.gao@gmail.com>
2019-06-23 23:45:01 -0700

    ran all correlations for MNI-tau vs. expression

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def return_fg_fits(fg_file, fg_folder):
     fg = FOOOFGroup()
     fg.load(fg_file, fg_folder)
     aps = fg.get_params('aperiodic_params') # get aperiodic parameters
-    # need to resave the old fooof files for this loading to work 
+    # need to resave the old fooof files for this loading to work
     #pks = fg.get_params('peak_params')
     pks = []
     err = fg.get_params('error')
@@ -59,38 +59,6 @@ def convert_knee_val(knee, exponent=2.):
     knee_freq = knee**(1./exponent)
     knee_tau = 1./(2*np.pi*knee_freq)
     return knee_freq, knee_tau
-
-# def compute_aggregate(df, aggr_func, group_col, loud=False):
-#     """
-#     Compute one- or two-level aggregate over the dataframe.
-#     """
-#     if len(group_col)==2:
-#         if loud: print('-> aggregating over %s.'%group_col[0])
-#         # group by specified column first and aggregate across specified types
-#         group_agg = pd.DataFrame()
-#         for group_id, group in df.groupby(group_col[0][0]):
-#             group_agg = group_agg.append(group.groupby(group_col[0][1]).aggregate(aggr_func))
-#
-#         if loud: print('---> aggregating over %s.'%group_col[1])
-#         grand_agg = group_agg.groupby(group_col[1])
-#
-#     elif len(group_col)==1 or type(group_col) is str:
-#         if type(group_col) is str:
-#             group_col = [group_col]
-#         # no first-level grouping, directly average across all electrodes
-#         if loud: print('-> aggregating over %s.'%group_col[0])
-#         group_agg = None
-#         grand_agg = df.groupby(group_col[0])
-#     return grand_agg, group_agg
-#
-# def compute_avg_sem(grouped_by, feature, aggr_func):
-#     """
-#     Computes group average via aggr_func, and standard error.
-#     grouped_by must be a grouped_by dataframe
-#     """
-#     agg_avg = grouped_by[feature].apply(eval(aggr_func))
-#     agg_sem = grouped_by[feature].apply(np.nanstd)/(grouped_by[feature].count()**0.5)
-#     return agg_avg, agg_sem
 
 def plot_psd_fits(f_axis, psds, chan, fgs, fg_labels):
     C_ORD = plt.rcParams['axes.prop_cycle'].by_key()['color']
@@ -125,6 +93,199 @@ def remove_spines(axis, bounds=['right', 'top']):
     for b in bounds:
         axis.spines[b].set_visible(False)
 
+
+### ------ for MNI analyses and visualization
+def compute_avg_dev(grouped_by, avg_dev_func):
+    """
+    Computes group average via aggr_func, and standard error.
+    grouped_by must be a grouped_by dataframe
+    """
+    agg_avg = grouped_by.agg(avg_dev_func[0])
+    agg_dev = grouped_by.agg(avg_dev_func[1])
+    return agg_avg, agg_dev
+
+def sort_per_lobe(sort_by_array, lobe_boundaries):
+    """
+    Return sorted indices, sorted by values in sort_by_array, given the constraint
+    that sorting is done per lobe, i.e., values within frontal lobe are sorted independent
+    of other regions.
+    """
+    reg_sorted_inds = []
+    for i,l in enumerate(lobe_boundaries.T):
+        reg_sorted_inds.append(sort_by_array[l[0]:l[1]].sort_values().index.values)
+        #inverse_map.append(sort_by_array[l[0]:l[1]].ar sort_values().index.values)
+
+    reg_sorted_inds = np.concatenate(reg_sorted_inds).astype(int)
+    return reg_sorted_inds
+
+
+def plot_agg_by_region(agg_avg, agg_dev, region_labels, group_agg=None, log_y=True, m_cfg=['o','k',10], sort_by='avg', plot_vert=False, shade_by_lobe=False):
+    """
+    Produce the aggregate per region plot.
+    """
+    C_ORD = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    # compute lobe boundaries
+    lobe_boundaries = np.sort(np.array([region_labels.groupby('Lobe')['Region #'].min(), region_labels.groupby('Lobe')['Region #'].max()]))
+
+    # sort axis per lobe
+    if sort_by is False:
+        # unsorted indices, keep as original
+        reg_sorted_inds = agg_avg.index.astype(int)
+    else:
+        # if 'avg', sort index by average value computed above
+        # otherwise sort_by is a pre-computed 2-col dataframe with region as indices and metric as values
+        if sort_by is 'avg': sort_by = agg_avg
+        reg_sorted_inds = sort_per_lobe(sort_by, lobe_boundaries)
+
+    # retrieve reverse mapping: given sorted ind
+    reverse_inds_map = pd.DataFrame(agg_avg.index.values, index=reg_sorted_inds)
+
+    # plot aggregate average & deviation
+    if plot_vert:
+        # plot vertically
+        plt.errorbar(agg_avg[reg_sorted_inds], agg_avg.index, xerr=agg_dev[reg_sorted_inds], fmt=m_cfg[0]+m_cfg[1], ms=m_cfg[2], alpha=0.5)
+    else:
+        plt.errorbar(agg_avg.index, agg_avg[reg_sorted_inds], yerr=agg_dev[reg_sorted_inds], fmt=m_cfg[0]+m_cfg[1], ms=m_cfg[2], alpha=0.5)
+
+    # plot per-subj average
+    if group_agg is not None:
+        for group_id, group in group_agg.groupby('region'):
+            # plot per-patient regional average
+            if plot_vert:
+                plt.plot(group, [reverse_inds_map.loc[group_id.astype(int)]]*len(group), 'k.', color=m_cfg[1], alpha=0.5, label=group_id, ms=m_cfg[2]/2)
+            else:
+                plt.plot([reverse_inds_map.loc[group_id.astype(int)]]*len(group), group, 'k.', color=m_cfg[1], alpha=0.5, label=group_id, ms=m_cfg[2]/2)
+
+    # label axes & ticks
+    # note the minus one because reg_sorted_inds are in terms of regions (1-38), but the array is 0-indexed
+    if plot_vert:
+        plt.yticks(region_labels['Region #'].values, region_labels['Region name'].values[reg_sorted_inds-1]);
+    else:
+        plt.xticks(region_labels['Region #'].values, region_labels['Region name'].values[reg_sorted_inds-1], rotation=300, ha='left', rotation_mode='anchor');
+
+    # setting log axis
+    if log_y: plt.xscale('log') if plot_vert else plt.yscale('log')
+
+    # setting axis limit
+    ax_lim = [region_labels['Region #'].min()-0.5, region_labels['Region #'].max()+0.5]
+    plt.ylim(ax_lim) if plot_vert else plt.xlim(ax_lim)
+
+    # plot shading for lobe
+    if shade_by_lobe:
+        if plot_vert:
+            XL = plt.xlim()
+            for i,l in enumerate(lobe_boundaries.T):
+                plt.fill_betweenx([l[0]-0.5,l[1]+0.5], XL[0], XL[1], alpha=0.15, color=C_ORD[i])
+        else:
+            YL = plt.ylim()
+            for i,l in enumerate(lobe_boundaries.T):
+                plt.fill_between([l[0]-0.5,l[1]+0.5], YL[0], YL[1], alpha=0.15, color=C_ORD[i])
+
+    # erase the top and right of box
+    remove_spines(plt.gca())
+    plt.tight_layout()
+
+def compute_distance_to_seed(df, lobe_seed_coors, seed_by_lobe=True, norm_by_lobe=False):
+    """
+    Compute Euclidian distance of every electrode position to the seed location in each lobe.
+    """
+    if seed_by_lobe:
+        # grab seed coordinate based on primary sensory coordinate of that lobe
+        seed_coor = lobe_seed_coors[df['lobe'].values.astype(int)]
+        seed_dist = np.sum(np.abs(df[['x_pos','y','z']].values - seed_coor)**2,axis=-1)**0.5
+    else:
+        # otherwise, grab the closest seed
+        # do a bunch of matrix gymnastics to compute distance to all 5 seeds in one line lol
+        seed_dist = np.sum(np.abs(np.repeat(df[['x_pos','y','z']].values[:,:,None],lobe_seed_coors.shape[0],2) \
+            - np.transpose(np.repeat(lobe_seed_coors[:,:,None], len(df_combined.index), -1), (2,1,0)))**2.,1)**0.5
+        closest_lobe_id = seed_dist.argmin(1) # id of lobe containing closest seed
+        seed_dist = seed_dist.min(1)
+
+    if norm_by_lobe:
+        normed_seed_dist = np.zeros_like(seed_dist)
+        # normalize distance from seed across regions in each lobe individually
+        for l in np.sort(np.unique(df['lobe'].values.astype(int))):
+            # norm by zscore
+            if norm_by_lobe is 'zscore':
+                normed_seed_dist[df['lobe']==l] = sp.stats.zscore(seed_dist[df['lobe']==l])
+            elif norm_by_lobe is 'max':
+                normed_seed_dist[df['lobe']==l] = seed_dist[df['lobe']==l]/seed_dist[df['lobe']==l].max()
+
+        seed_dist = normed_seed_dist
+
+    df_out = df.copy()
+    df_out.insert(len(df_out.columns), 'seed_dist', seed_dist)
+    return df_out
+
+def plot_from_seeddist(df_w_dist, feature, lobe_id, log_y=True, hold_axis=True):
+    """
+    Plot distance from primary area (seed) against the value of interest (feature).
+    """
+    C_ORD = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    xy_axis_frac = (0.45, 0.85)
+    ax = plt.subplot(2,3,1)
+    plt.plot(df_w_dist['seed_dist'], df_w_dist[feature], '.k', alpha=0.5, mec='none')
+    if log_y: plt.yscale('log')
+    remove_spines(plt.gca())
+    rho, pv = sp.stats.spearmanr(df_w_dist['seed_dist'], df_w_dist[feature], nan_policy='omit')
+    s = r'$\rho$ = %.3f '%rho + np.sum(pv<=np.array([0.05, 0.01, 0.001]))*'*'
+    plt.annotate(s, xy=xy_axis_frac, xycoords='axes fraction', size=14)
+    plt.xticks([]);plt.yticks([])
+    plt.title('All')
+    XL = plt.xlim(); YL = plt.ylim()
+
+    for k,v in lobe_id.items():
+        ax =plt.subplot(2,3,v+2)
+        x,y = df_w_dist['seed_dist'][df_w_dist['lobe']==v], df_w_dist[feature][df_w_dist['lobe']==v]
+        plt.plot(x,y, '.', color=C_ORD[v], alpha=0.7, mec='none')
+        if log_y: plt.yscale('log')
+        remove_spines(plt.gca())
+        #ax.spines['right'].set_visible(False); ax.spines['top'].set_visible(False)
+        if hold_axis:
+            plt.xlim(XL);plt.ylim(YL)
+            plt.xticks([]);plt.yticks([])
+
+        rho, pv = sp.stats.spearmanr(x, y, nan_policy='omit')
+        s = r'$\rho$ = %.3f '%rho + np.sum(pv<=np.array([0.05, 0.01, 0.001]))*'*'
+        plt.annotate(s, xy=xy_axis_frac, xycoords='axes fraction', size=14)
+        plt.title(k)
+
+    plt.subplot(2,3,4)
+    plt.xticks([0,1]);plt.yticks([0.01,0.1,1])
+    plt.xlabel('Distance from Primary Area'); plt.ylabel(feature)
+    plt.tight_layout()
+
+def apply_affine(f_affine, coord, ijk2xyz):
+    """
+    Apply forward (index to MNI coor) or reverse (MNI to index) affine transformation.
+    """
+    M_aff, M_trsl = f_affine[:3,:3], f_affine[:3,-1]
+    if ijk2xyz:
+        # index to MNI coordinate
+        return np.dot(M_aff,coord)+M_trsl
+    else:
+        # MNI coordinate to index
+        return np.dot(np.linalg.inv(M_aff),coord-M_trsl)
+
+def ecog_gene_corr(df_combined, df_anat, avg_dev_func, ephys_feat='tau', anat_feat='T1T2', age_subset=None):
+    """
+    Return ephys and anatomy feature correlation and arrays for parcel average and per-electrode.
+    """
+    # if no age restricted indices are specified, take all values
+    if age_subset is None: age_subset=df_combined['age']>0
+    # parcel-level average
+    ephys_avg = df_combined[age_subset].groupby('gparcel').agg(avg_dev_func[0])[ephys_feat]
+    ephys_dev = df_combined[age_subset].groupby('gparcel').agg(avg_dev_func[1])[ephys_feat]
+    anat_avg = df_anat[anat_feat][ephys_avg.index]
+    rho_agg, pv_agg = sp.stats.spearmanr(ephys_avg, anat_avg, nan_policy='omit')
+
+    # return electrode level
+    anat_elec = np.array([df_anat.iloc[int(p_i-1)][anat_feat] if ~np.isnan(p_i) else np.nan for p_i in df_combined['gparcel']])[age_subset]
+    ephys_elec = df_combined[ephys_feat][age_subset]
+    rho_elec, pv_elec = sp.stats.spearmanr(ephys_elec, anat_elec, nan_policy='omit')
+    return ephys_avg, ephys_dev, anat_avg, (rho_agg, pv_agg), ephys_elec, anat_elec, (rho_elec, pv_elec)
+
+### ------ for handling spiking analyses
 
 def acf_events(spks, t_max, dt=0.001, norm=True, remove_zero=False):
     # get time bins on positive side and reflect around 0
